a82a14b048f0ba07dccae17b7e90b697facb4334
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-02 14:03:40 -0500

    building app

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
+  const [searchQueary, setSearchQuery] = useState('')
+
+  const onChangeSearch = query => setSearchQuery(query)
   return (
-    <View style={styles.container}>
-      <Text> My React Native blank canvas</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar placeholder='Search' onChangeText={onChangeSearch} value={searchQueary} />
+      </View>
+      <View style={styles.list}>
+        <Text> List </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
+    //Only for Androind devices
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    // flex: .05,
+    backgroundColor: 'green',
+    padding: 16,
     justifyContent: 'center',
   },
+  list: {
+    flex: 1,
+    backgroundColor: 'blue',
+    padding: 16,
+  }
 });
