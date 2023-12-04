4db691567524be3d27944777811ba001af427628
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-03 23:17:03 -0500

    updating code

--- App.js
+++ App.js
@@ -1,40 +1,10 @@
-
-import { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import RestaurantsScreen from './src/features/restaurants/screens/restaurant.screens';
 
 export default function App() {
-  const [searchQueary, setSearchQuery] = useState('')
 
-  const onChangeSearch = query => setSearchQuery(query)
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Searchbar placeholder='Search' onChangeText={onChangeSearch} value={searchQueary} />
-      </View>
-      <View style={styles.list}>
-        <Text> List </Text>
-      </View>
-    </SafeAreaView>
+    <RestaurantsScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    //Only for Androind devices
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    // flex: .05,
-    backgroundColor: 'green',
-    padding: 16,
-    justifyContent: 'center',
-  },
-  list: {
-    flex: 1,
-    backgroundColor: 'blue',
-    padding: 16,
-  }
-});
+
