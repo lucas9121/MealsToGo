bdfba0c1bd2cd090b0c8f46a595e6721150a9f43
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-06 20:33:56 -0500

    updating code

--- src/features/restaurants/screens/restaurant.screens.js
+++ src/features/restaurants/screens/restaurant.screens.js
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import styled from "styled-components/native";
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import { SafeAreaView, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  background-color: black;
+  background-color: ${(props) => props.theme.colors.bg.dark};
   /* Avoids terminal warning */
   ${StatusBar.currentHeight &&
   `margin-top: ${StatusBar.currentHeight}px;`}; /* Only for Android devices */
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const ListContainer = styled.View`
   flex: 1;
-  background-color: blue;
-  padding: 16px;
+  background-color: ${(props) => props.theme.colors.bg.secondary};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export default function RestaurantsScreen() {
@@ -42,12 +42,3 @@ export default function RestaurantsScreen() {
     </SafeArea>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    //Only for Androind devices
-    marginTop: StatusBar.currentHeight,
-  },
-});
