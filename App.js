802ae1905b52805b2735961d4aeeab5b078ca9d6
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-06 19:57:29 -0500

    adding theme

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurant.screens";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
-  return <RestaurantsScreen />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RestaurantsScreen />
+    </ThemeProvider>
+  );
 }
