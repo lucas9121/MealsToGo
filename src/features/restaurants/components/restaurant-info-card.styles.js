af6378375c71df21700702401495d34b0e2d2c1e
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-10 23:03:36 -0500

    updating code

--- src/features/restaurants/components/restaurant-info-card.styles.js
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { View, Image } from "react-native";
 import { Card } from "react-native-paper";
 
 export const CardCover = styled(Card.Cover)`
@@ -15,25 +14,25 @@ export const Address = styled.Text`
   font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
-export const Section = styled(View)`
+export const Section = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   padding: ${(props) => props.theme.space[1]} 0;
 `;
 
-export const Rating = styled(View)`
+export const Rating = styled.View`
   flex-direction: row;
   align-items: center;
 `;
 
-export const Open = styled(View)`
+export const Open = styled.View`
   flex-direction: row;
   align-items: center;
   gap: 8px;
 `;
 
-export const Icon = styled(Image)`
+export const Icon = styled.Image`
   width: 20px;
   height: 20px;
 `;
