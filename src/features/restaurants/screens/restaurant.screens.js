0ce9ad094b99ec0c3d14d72b3338fb8b4549874b
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-11 22:10:05 -0500

    updating code

--- src/features/restaurants/screens/restaurant.screens.js
+++ src/features/restaurants/screens/restaurant.screens.js
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import styled from "styled-components/native";
-import { SafeAreaView, StatusBar, FlatList } from "react-native";
+import { SafeAreaView, StatusBar, FlatList, Platform } from "react-native";
 import { Searchbar } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../components/spacer/spacer.component";
-import { colors } from "../../../infrastructure/theme/colors";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  background-color: ${(props) => props.theme.colors.bg.dark};
+  /* background-color: ${(props) => props.theme.colors.bg.dark}; */
   /* Avoids terminal warning */
   ${StatusBar.currentHeight &&
   `margin-top: ${StatusBar.currentHeight}px;`}; /* Only for Android devices */
@@ -19,11 +18,12 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const ListContainer = styled.View`
-  flex: 1;
-  background-color: ${(props) => props.theme.colors.bg.secondary};
-  padding: ${(props) => props.theme.space[3]};
-`;
+// attributes function allows me to give specific props to default value, which is Flatlist in this case
+const ListContainer = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 const restaurantAPI = Array.from(new Array(15));
 restaurantAPI.forEach((ele, idx) => {
@@ -37,6 +37,9 @@ export default function RestaurantsScreen() {
 
   return (
     <SafeArea>
+      <StatusBar
+        barStyle={Platform.OS === "ios" ? "dark-content" : "light-content"}
+      />
       <SearchContainer>
         <Searchbar
           placeholder="Search"
@@ -44,7 +47,7 @@ export default function RestaurantsScreen() {
           value={searchQueary}
         />
       </SearchContainer>
-      <FlatList
+      <ListContainer
         data={restaurantAPI}
         renderItem={() => (
           <Spacer position="bottom" size="large">
@@ -52,10 +55,6 @@ export default function RestaurantsScreen() {
           </Spacer>
         )}
         keyExtractor={(item) => item.name}
-        contentContainerStyle={{
-          backgroundColor: `${colors.bg.secondary}`,
-          padding: 16,
-        }}
       />
     </SafeArea>
   );
