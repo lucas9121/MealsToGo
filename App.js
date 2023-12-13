801f973624eabbfaa44d41fcd4ca3b2f7e2b7d18
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-12 23:26:36 -0500

    updating code

--- App.js
+++ App.js
@@ -9,12 +9,35 @@ import { Lato_400Regular, Lato_700Bold } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurant.screens";
 import SettingsScreen from "./src/features/restaurants/screens/settings.screen";
 import MapScreen from "./src/features/restaurants/screens/map.screen";
 
 const Tab = createBottomTabNavigator();
+
+// will choose icon based on focused
+const getIconName = (route, focused) => {
+  switch (route.name) {
+    case "Restaurants":
+      return (iconName = focused ? "restaurant" : "restaurant-outline");
+    case "Map":
+      return (iconName = focused ? "map" : "map-outline");
+    default:
+      return (iconName = focused ? "settings" : "settings-outline");
+  }
+};
+
+// Navigator icons
+const createScreenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, size, color }) => {
+    const iconName = getIconName(route, focused);
+    return <Ionicons name={iconName} size={size} color={color} />;
+  },
+  tabBarActiveTintColor: "tomato",
+  tabBarInactiveTintColor: "gray",
+});
 
 export default function App() {
   const [oswaldLoaded] = useFonts({
@@ -34,7 +57,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={createScreenOptions}>
           <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
           <Tab.Screen name="Map" component={MapScreen} />
           <Tab.Screen name="Settings" component={SettingsScreen} />
