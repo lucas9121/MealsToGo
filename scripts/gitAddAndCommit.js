165d1652b8cd74e0b0df607a4e0d8efb51a59bdc
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-13 20:43:14 -0500

    creating git command file

--- scripts/gitAddAndCommit.js
+++ scripts/gitAddAndCommit.js
@@ -1,22 +1,19 @@
-// const fs = require('fs');
-// const git = require('git');
+// Execute commands synchronously
 const { execSync } = require('child_process');
 
-const add = () => {
-  execSync('git add .', { stdio: 'inherit' });
-};
+const add = () => (
+  // stdio: 'inherit' ensures that the command's output is printed in the terminal
+  execSync('git add .', { stdio: 'inherit' })
+);
 
-const commit = (message) => {
-  execSync(`git commit -m "${message}"`, { stdio: 'inherit' });
-  // git.commit(message);
-};
+const commit = (message) => (
+  execSync(`git commit -m "${message}"`, { stdio: 'inherit' })
+);
 
 const command = () => {
   const message = process.argv[2] || "updating code"
   add()
   commit(message)
-  console.log(`Received custom message: ${message}`)
-  // const customMessage = process.argv[2]
 }
 
-command()
+command().then(() => console.log(`git updated`)).catch((err) => console.error(err))
