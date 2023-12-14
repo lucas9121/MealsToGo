b43953988a49830b0fc3af6726bdc2e6e5e81160
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-14 16:55:31 -0500

    updating code

--- src/services/restaurants/mock/restaurants.service.js
+++ src/services/restaurants/mock/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "."; //pulling from index
+import camelize from "camelize"; // camelcase seperate words. Adds consistency from API being returned.
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
@@ -7,9 +8,14 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+};
 restaurantsRequest()
-  .then((result) => {
-    console.log(result);
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(camelize(transformedResponse));
     console.log("it worked!!!");
   })
   .catch((err) => {
