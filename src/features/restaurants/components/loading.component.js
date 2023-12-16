46cbc9e1b2d0df90e28eb48bce01246d2d876bc6
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-16 16:30:58 -0500

    updating code

--- src/features/restaurants/components/loading.component.js
+++ src/features/restaurants/components/loading.component.js
@@ -1,14 +1,17 @@
 import { ActivityIndicator } from "react-native-paper";
+import styled from "styled-components";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { colors } from "../../../infrastructure/theme/colors";
 
+const Loading = styled(ActivityIndicator)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const LoadingComponent = () => (
   <SafeArea>
-    <ActivityIndicator
-      size={"large"}
-      color={colors.ui.blue}
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    />
+    <Loading size={"large"} color={colors.ui.blue} />
   </SafeArea>
 );
