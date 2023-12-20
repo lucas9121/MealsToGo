cb1aa7b93db5aa46284268b7dfa30a31213ca603
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-19 22:23:30 -0500

    updating code

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -5,35 +5,59 @@ import { locationRequest, locationTransform } from "./location.service";
 export const LocationContext = createContext();
 
 export const LocationContextProvider = ({ children }) => {
-  const [keyword, setKeyword] = useState("San Francisco");
+  const [keyword, setKeyword] = useState("");
   const [location, setLocation] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [initialLoad, setInitialLoad] = useState(true);
 
   const onSearch = (searchKeyword) => {
     setIsLoading(true);
     setKeyword(searchKeyword);
   };
 
-  const runSearch = async () => {
+  const handleSearchResult = (req) => {
+    const result = locationTransform(req);
+    setLocation((prevLocation) => ({
+      ...prevLocation,
+      lat: result.lat,
+      lng: result.lng,
+    }));
+  };
+
+  const handleSearchError = (err) => {
+    setIsLoading(false);
+    setError(err);
+    setKeyword("");
+  };
+
+  const performInitialLoad = async () => {
     try {
+      const request = await locationRequest("san francisco");
+      handleSearchResult(request);
+      setIsLoading(false);
+      setInitialLoad(false);
+      setKeyword("");
+    } catch (error) {
+      handleSearchError(error);
+    }
+  };
+
+  const fetchLocationData = async () => {
+    try {
+      if (initialLoad) return await performInitialLoad();
       if (!keyword.length) return; //end here if no search word
       const request = await locationRequest(keyword.toLowerCase());
-      const result = locationTransform(request);
-      setLocation((prevLocation) => ({
-        ...prevLocation,
-        lat: result.lat,
-        lng: result.lng,
-      }));
+      handleSearchResult(request);
       setIsLoading(false);
+      setKeyword("");
     } catch (error) {
-      setIsLoading(false);
-      setError(error);
+      handleSearchError(error);
     }
   };
 
   useEffect(() => {
-    runSearch();
+    fetchLocationData();
   }, [keyword]);
 
   return (
