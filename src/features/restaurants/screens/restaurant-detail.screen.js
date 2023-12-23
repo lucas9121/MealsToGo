217b205c638af082b26ea44f44ae80e862a38426
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-23 02:35:18 -0500

    updating code

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,7 +1,6 @@
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantMenu } from "../components/restaurant-menu";
-import { Spacer } from "../components/spacer/spacer.component";
 
 export const RestaurantDetailScreen = ({ route }) => {
   const { restaurant } = route.params; // coming from restaurant screens second argument from navigate.
