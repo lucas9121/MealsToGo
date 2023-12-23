00d3431a09909864bde0666ef61024b465b5539c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-12-23 01:18:10 -0500

    adding menu list

---
+++ src/features/restaurants/components/restaurant-menu.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { List } from "react-native-paper";
+
+export const RestaurantMenu = () => {
+  const [breakfastExpanded, setBreakfastExpanded] = useState(true);
+  const [lunchExpanded, setLunchExpanded] = useState(true);
+  const [dinnerExpanded, setDinnerExpanded] = useState(true);
+
+  const handlePressBreakfast = () => setBreakfastExpanded(!breakfastExpanded);
+  const handlePressLunch = () => setLunchExpanded(!lunchExpanded);
+  const handlePressDinner = () => setDinnerExpanded(!dinnerExpanded);
+  return (
+    <List.Section title="Accordions">
+      <List.Accordion
+        title="Breakfast"
+        left={(props) => <List.Icon {...props} icon="bread-slice" />}
+        expanded={breakfastExpanded}
+        onPress={handlePressBreakfast}
+      >
+        <List.Item title="Eggs Bennedict" />
+        <List.Item title="Classic Breakfast" />
+      </List.Accordion>
+      <List.Accordion
+        title="Lunch"
+        left={(props) => <List.Icon {...props} icon="hamburger" />}
+        expanded={lunchExpanded}
+        onPress={handlePressLunch}
+      >
+        <List.Item title="Burger w/ Fries" />
+        <List.Item title="Steak Sandwich" />
+        <List.Item title="Mushroom Soup" />
+      </List.Accordion>
+      <List.Accordion
+        title="Dinner"
+        left={(props) => <List.Icon {...props} icon="food-variant" />}
+        expanded={dinnerExpanded}
+        onPress={handlePressDinner}
+      >
+        <List.Item title="Penne alla Vodka" />
+        <List.Item title="Lasagna" />
+        <List.Item title="Salmon" />
+        <List.Item title="Veal Cutlet" />
+      </List.Accordion>
+      <List.Accordion
+        title="Drinks"
+        left={(props) => <List.Icon {...props} icon="cup" />}
+      >
+        <List.Item title="Coffee" />
+        <List.Item title="Teat" />
+        <List.Item title="Modello" />
+        <List.Item title="Coke" />
+      </List.Accordion>
+    </List.Section>
+  );
+};
