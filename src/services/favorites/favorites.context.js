b1daeb240c8683bdaa51e658e75659d5e5c253ff
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-01-06 15:40:02 -0500

    building favorites

---
+++ src/services/favorites/favorites.context.js
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+
+export const FavoritesContext = createContext();
+
+export const FavoritesContextProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  const add = (restaurant) => {
+    if (favorites.find(restaurant)) return;
+    setFavorites((prevFavorites) => [...prevFavorites, restaurant]);
+  };
+
+  const remove = (restaurant) => {
+    const newFavorites = favorites.filter(
+      (item) => item.placeId !== restaurant.placeId
+    );
+    setFavorites(newFavorites);
+  };
+  return (
+    <FavoritesContext.Provider
+      value={{
+        favorites,
+        addToFavorites: add,
+        removeFromFavorites: remove,
+      }}
+    >
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
