5a9acc0f13cc541d8c4d56edf9821b2574d87cb1
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-01-06 16:19:08 -0500

    updating code

---
+++ src/components/favorite/favorite.component.js
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import { AntDesign } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+
+import { FavoritesContext } from "../../services/favorites/favorites.context";
+
+const FavoriteButton = styled(TouchableOpacity)`
+  border-color: #20232a;
+  position: aboslute;
+  top: 10px;
+  right: 10px;
+  z-index: 9;
+`;
+
+export const Favorite = () => {
+  const { favorites, addToFavorites, removeFromFavorites } =
+    useContext(FavoritesContext);
+
+  return <FavoriteButton>Heart</FavoriteButton>;
+};
