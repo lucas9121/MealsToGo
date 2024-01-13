88178aa9cec6ccc571684a7ea3ceba4a351c5e9c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-01-13 16:41:04 -0500

    testing authentication

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// https://firebase.google.com/docs/web/setup#available-libraries
 import {
   useFonts,
   Oswald_300Light,
@@ -14,7 +20,33 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
 import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcInD3zDLAfau8COt8CrfZleq1XSOz29U",
+  authDomain: "mealstogo-2af32.firebaseapp.com",
+  projectId: "mealstogo-2af32",
+  storageBucket: "mealstogo-2af32.appspot.com",
+  messagingSenderId: "1005770049477",
+  appId: "1:1005770049477:web:99f64503629cedd34cadac",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(auth, "<email>", "<password>")
+        .then((userCredential) => {
+          setIsAuthenticated(true);
+        })
+        .catch((err) => {
+          console.error("failed to authenticate: ", err);
+        });
+    }, 2000);
+  }, []);
   const [oswaldLoaded] = useFonts({
     Oswald_300Light,
     Oswald_400Regular,
@@ -29,6 +61,9 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
+
   return (
     <>
       <ThemeProvider theme={theme}>
