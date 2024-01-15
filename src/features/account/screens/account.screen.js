5b9d76a44d81b3efb276c3bfe364350e9e296a12
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-01-15 16:13:28 -0500

    adding button

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,7 +1,30 @@
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+} from "../components/account.styles";
+import { AuthButton } from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
-export const AccountScreen = () => (
+export const AccountScreen = ({ navigation }) => (
   <AccountBackground>
     <AccountCover />
+    <AccountContainer>
+      <AuthButton
+        icon="lock-open-outline"
+        mode="contained"
+        onPress={() => navigation.navigate("Login")}
+      >
+        Login
+      </AuthButton>
+      <Spacer size="large" />
+      <AuthButton
+        icon="lock-open-outline"
+        mode="contained"
+        onPress={() => navigation.navigate("Register")}
+      >
+        Register
+      </AuthButton>
+    </AccountContainer>
   </AccountBackground>
 );
