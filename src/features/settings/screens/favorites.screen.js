a1f523c1242231c2a219655bae31f88edac9e47a
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-01-23 17:29:55 -0500

    updating code

--- src/features/settings/screens/favorites.screen.js
+++ src/features/settings/screens/favorites.screen.js
@@ -1,10 +1,12 @@
 import { useContext } from "react";
+import { TouchableOpacity, View } from "react-native";
+import { Button } from "react-native-paper";
+
 import { FavoritesContext } from "../../../services/favorites/favorites.context";
-import { TouchableOpacity, FlatList, Button } from "react-native";
 import { RestaurantInfoCard } from "../../restaurants/components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
-
+import { AuthButton } from "../../account/components/account.styles";
 import { Text } from "../../../components/typography/text.component";
 import { RestaurantList } from "../../restaurants/components/restaurant-list.styles";
 
@@ -12,6 +14,24 @@ export default function FavoritesScreen({ navigation }) {
   const { favorites } = useContext(FavoritesContext);
   return favorites.length ? (
     <SafeArea style={{ justifyContent: "center" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingRight: 16,
+        }}
+      >
+        <Button
+          title="Back"
+          icon="arrow-left"
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
+        <Text variant="title">Favorites</Text>
+        <Spacer position="right" size="large" />
+      </View>
       <RestaurantList
         style={{ height: 2 }}
         data={favorites}
@@ -32,12 +52,6 @@ export default function FavoritesScreen({ navigation }) {
           );
         }}
         keyExtractor={(item) => item.name}
-      />
-      <Button
-        onPress={() => {
-          navigation.goBack();
-        }}
-        title="Back"
       />
     </SafeArea>
   ) : (
