184b999873778f89ed3b4602bea378b85c01b778
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-01-23 10:56:46 -0500

    updating code

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  /* background-color: ${(props) => props.theme.colors.bg.dark}; */
+  background-color: ${(props) => props.theme.colors.bg.primary};
   /* Avoids terminal warning */
   ${StatusBar.currentHeight &&
   `margin-top: ${StatusBar.currentHeight}px;`}; /* Only for Android devices */
