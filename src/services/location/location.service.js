d918034e8e19dafe5bc55d21fb03d80a572d8e2b
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-02-18 22:32:21 -0500

    adding server request

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,6 +1,22 @@
 import camelize from "camelize";
 
-export const locationRequest = (searchTerm) => {};
+export const locationRequest = async (searchTerm) => {
+  try {
+    const res = await fetch(
+      `http://localhost:5001/mealstogo-2af32/us-central1/geocode?city=${searchTerm}`
+    );
+
+    if (!res.ok) {
+      console.log("there was an error");
+      throw new Error(`Network response was not ok: ${res.statusText}`);
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("Error during fetch:", error);
+    throw error;
+  }
+};
 
 export const locationTransform = (result) => {
   const formattedResponse = camelize(result);
