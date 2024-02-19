5350b759c76326c6131db970708b95f06cc3824c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-02-19 16:31:32 -0500

    adding google maps api

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,7 +1,24 @@
 const { locations: locationsMock } = require("./geocode.mock");
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = request.query;
-  const locationMock = locationsMock[city.toLowerCase()];
-  response.json(locationMock);
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = request.query;
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    return response.json(locationMock);
+  }
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: process.env.GOOGLE_MAPS_API_KEY,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      console.error(e.response.data.error_message);
+      response.status(400).send(e.response.data.error_message);
+    });
 };
--- functions/index.js
+++ functions/index.js
@@ -11,14 +11,21 @@ const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const { geocodeRequest } = require("./geocode");
 const { placesRequest } = require("./places");
+const { defineString } = require("firebase-functions/params");
+
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+
+defineString("GOOGLE_MAPS_API_KEY");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
 exports.geocode = onRequest((request, response) => {
-  geocodeRequest(request, response);
+  geocodeRequest(request, response, client);
 });
 
 exports.placesNearby = onRequest((request, response) => {
-  placesRequest(request, response);
+  placesRequest(request, response, client);
 });
