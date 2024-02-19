de181d475e843dc320b6d007d393ae90486e14f7
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-02-18 22:25:47 -0500

    adding places request function

--- functions/index.js
+++ functions/index.js
@@ -10,10 +10,15 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const { geocodeRequest } = require("./geocode");
+const { placesRequest } = require("./places");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
 exports.geocode = onRequest((request, response) => {
   geocodeRequest(request, response);
+});
+
+exports.placesNearby = onRequest((request, response) => {
+  placesRequest(request, response);
 });
