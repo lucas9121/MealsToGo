351e0e618b6109ec763b2d7f45f2c9cdb7f38e6a
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-02-19 16:12:23 -0500

    adding path

--- scripts/updateFirebaseConfig.js
+++ scripts/updateFirebaseConfig.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
+const path = require("path");
+
+const envPath = path.resolve(__dirname, "..", ".env");
 
 // Load environment variables
-require("dotenv").config();
+require("dotenv").config({ path: envPath });
 
 // Read firebase.json file
 const firebaseConfig = require("../firebase.json");
 
 // Update the host value in the functions emulator configuration
-firebaseConfig.emulators.functions.host = process.env.FIREBASE_FUNCTIONS_HOST;
+firebaseConfig.emulators.functions.host = process.env.FUNCTIONS_HOST;
 
 // Write the updated configuration back to firebase.json
 fs.writeFileSync("../firebase.json", JSON.stringify(firebaseConfig, null, 2));
