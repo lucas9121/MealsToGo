b2653c169954a8e42955b0537c14d7811324c65b
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-02-20 21:34:32 -0500

    fixing path

--- scripts/updateFirebaseConfig.js
+++ scripts/updateFirebaseConfig.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const envPath = path.resolve(__dirname, "..", ".env");
+const envPath = path.resolve(__dirname, "..", ".env.local");
 
 // Load environment variables
 require("dotenv").config({ path: envPath });
@@ -11,6 +11,7 @@ const firebaseConfig = require("../firebase.json");
 
 // Update the host value in the functions emulator configuration
 firebaseConfig.emulators.functions.host = process.env.FUNCTIONS_HOST;
+console.log(process.env.FUNCTIONS_HOST);
 
 // Write the updated configuration back to firebase.json
 fs.writeFileSync("../firebase.json", JSON.stringify(firebaseConfig, null, 2));
