cfc2a69dd73ab9c8afaf9bbb65f8347476e3157c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-02-20 21:12:01 -0500

    building places API function

--- functions/places/index.js
+++ functions/places/index.js
@@ -1,7 +1,26 @@
 const { mocks, addMockImage } = require("./mock");
-module.exports.placesRequest = (req, res) => {
-  const { location } = req.query;
-  const data = mocks[location];
-  if (data) data.results.map(addMockImage);
-  res.json(data);
+module.exports.placesRequest = (request, response, client) => {
+  const { location, mock } = request.query;
+  if (mock === "true") {
+    const data = mocks[location];
+    if (data) data.results.map(addMockImage);
+    return response.json(data);
+  }
+  client
+    .placesNearby({
+      params: {
+        location,
+        radius: 1500, //set in meters
+        type: "restaurant",
+        key: process.env.GOOGLE_MAPS_API_KEY,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      console.error(e.response.data.error_message);
+      response.status(400).send(e.response.data.error_message);
+    });
 };
