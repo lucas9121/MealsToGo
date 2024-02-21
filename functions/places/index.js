35323b71fa6880f464543aa11fe9e570dbe6c647
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-02-20 22:18:48 -0500

    adding google images

--- functions/places/index.js
+++ functions/places/index.js
@@ -1,4 +1,21 @@
 const { mocks, addMockImage } = require("./mock");
+
+const addGoogleImage = (restaurant) => {
+  if (!restaurant.photos) {
+    restaurant = addMockImage;
+    return restaurant;
+  }
+  const ref = restaurant.photos[0].photo_reference;
+  if (!ref) {
+    restaurant = addMockImage;
+    return restaurant;
+  }
+  restaurant.photos = [
+    `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${process.env.GOOGLE_MAPS_API_KEY}`,
+  ];
+  return restaurant;
+};
+
 module.exports.placesRequest = (request, response, client) => {
   const { location, mock } = request.query;
   if (mock === "true") {
@@ -13,11 +30,12 @@ module.exports.placesRequest = (request, response, client) => {
         radius: 1500, //set in meters
         type: "restaurant",
         key: process.env.GOOGLE_MAPS_API_KEY,
+        fields: "photos",
       },
       timeout: 1000,
     })
     .then((res) => {
-      res.data.results = res.data.results.map(addMockImage);
+      res.data.results = res.data.results.map(addGoogleImage);
       return response.json(res.data);
     })
     .catch((e) => {
