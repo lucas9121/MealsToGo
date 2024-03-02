378e2fce4bdf85e30beaf6647a4aa98627e454f3
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-03-02 14:08:50 -0500

    updating code

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,11 @@
+import { Platform } from "react-native";
 const livePlacesHost = process.env.EXPO_PUBLIC_PLACES_NEARBY_URL;
 const liveLocationHost = process.env.EXPO_PUBLIC_GEOCODE_URL;
 const localHost = process.env.EXPO_PUBLIC_LOCAL_URL;
+const isDevelopment = process.env.NODE_ENV === "development";
+const isAndroid = Platform.OS === "android"; // android can't use http
 export const restaurantHost =
-  process.env.NODE_ENV === "development" ? localHost : livePlacesHost;
+  !isDevelopment || isAndroid ? livePlacesHost : localHost;
 export const locationtHost =
-  process.env.NODE_ENV === "development" ? localHost : liveLocationHost;
+  !isDevelopment || isAndroid ? liveLocationHost : localHost;
+export const isMock = process.env.NODE_ENV === "development" ? true : false;
