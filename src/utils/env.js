e9cb9ad408b70f8ab735a2f1c2b4747dffef91b9
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-03-02 14:05:55 -0500

    Revert "updating code"
    This reverts commit 4c845eb94a250d433671929cf3b2c368138cdb64.

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,11 +1,7 @@
-import { Platform } from "react-native";
 const livePlacesHost = process.env.EXPO_PUBLIC_PLACES_NEARBY_URL;
 const liveLocationHost = process.env.EXPO_PUBLIC_GEOCODE_URL;
 const localHost = process.env.EXPO_PUBLIC_LOCAL_URL;
-const isDevelopment = process.env.NODE_ENV === "development";
-const isAndroid = Platform.OS === "android"; // android can't use http
 export const restaurantHost =
-  !isDevelopment || isAndroid ? livePlacesHost : localHost;
+  process.env.NODE_ENV === "development" ? localHost : livePlacesHost;
 export const locationtHost =
-  !isDevelopment || isAndroid ? liveLocationHost : localHost;
-export const isMock = process.env.NODE_ENV === "development" ? true : false;
+  process.env.NODE_ENV === "development" ? localHost : liveLocationHost;
