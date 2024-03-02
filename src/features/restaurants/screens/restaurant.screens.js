d7a74ad37ab0863536129a03af05d43a7ae5159b
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-03-02 13:49:03 -0500

    adding error message

--- src/features/restaurants/screens/restaurant.screens.js
+++ src/features/restaurants/screens/restaurant.screens.js
@@ -3,9 +3,11 @@ import { TouchableOpacity } from "react-native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
 import { FavoritesBar } from "../../../components/favorite/favorites-bar.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { FadeInView } from "../components/animations/fade.animation";
+import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { FavoritesContext } from "../../../services/favorites/favorites.context";
 import { LoadingComponent } from "../components/loading.component";
@@ -14,7 +16,8 @@ import { RestaurantList } from "../components/restaurant-list.styles";
 
 // prop comes from restaurant navigator
 export default function RestaurantsScreen({ navigation }) {
-  const { isLoading, restaurants } = useContext(RestaurantsContext); // popuplates the ammount of restaurants determined in RestaurantsContext array
+  const { error: locationError } = useContext(LocationContext);
+  const { isLoading, restaurants, error } = useContext(RestaurantsContext); // popuplates the ammount of restaurants determined in RestaurantsContext array
   const { favorites } = useContext(FavoritesContext);
   const [isToggled, setIsToggled] = useState(false);
 
@@ -29,6 +32,10 @@ export default function RestaurantsScreen({ navigation }) {
       {isToggled && (
         <FavoritesBar favorites={favorites} onNavigate={navigation.navigate} />
       )}
+      {!!error ||
+        (!!locationError && ( // transforms empty object into boolean
+          <Text variant="error">Oh no! Something went wrong.</Text>
+        ))}
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
